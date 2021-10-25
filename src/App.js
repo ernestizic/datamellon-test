448a6394e2473b0c0eb40133ffd8324adba1999e
ernestizic <ieifeanyichukwu@gmail.com>
2021-10-25 16:07:18 +0100

    changes in useEffect

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [salesData, setSalesData] = useState([]);
   const [salesDataCopy, setSalesDataCopy] = useState([]);
-  const [chartData, setChartData] = useState({});
+  // const [chartData, setChartData] = useState({});
 
   const [currentPage, setCurrentPage] = useState(1);
   const [dataPerPage] = useState(10);
@@ -40,6 +40,7 @@ function App() {
     }
   }
 
+  // sort by profit in asc and desc order
   const sortasc =()=> {
     setSalesDataCopy([...salesDataCopy.sort((a, b)=> a.Profit - b.Profit)])
   }
@@ -47,6 +48,7 @@ function App() {
     setSalesDataCopy([...salesDataCopy.sort((a, b)=> b.Profit - a.Profit)])
   }
 
+  // sort by year in asc and desc order
   const sortYearAsc =()=> {
     setSalesDataCopy([...salesDataCopy.sort((a, b)=> new Date(a['Order Date']) - new Date(b['Order Date']) )])
   }
@@ -69,35 +71,56 @@ function App() {
   const myData = salesData.map(sales => {
     return {x: sales['Order Date'], y: sales['Profit']}
   }).sort((a, b)=> new Date(a.x) - new Date(b.x) )
-
+  console.log(salesData)
 
   ///////////// useEffect  /////////////////////
   useEffect(() => {
     fetchSalesData();
-
-    setChartData({
-      labels: ["Office Supplies", "Technology", "Furniture"],
-      datasets: [
-        {
-          label: "Sales Data",
-          data: [offLen, techLen, furLen],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-          ],
-        },
-      ],
-    });
-
-  }, [furLen, offLen, techLen]);
+  }, []);
 
   useEffect(() => {
     setSalesDataCopy(salesData)
   }, [salesData])
   
 // ///////////////////////////////////////////////////
+
+
+///////////////////////// Fetching data from API  //////////
+
+const fetchSalesData = async () => {
+  const res = await fetch(
+    "https://g54qw205uk.execute-api.eu-west-1.amazonaws.com/DEV/stub",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ angular_test: "angular-developer" }),
+    }
+  );
+  const data = await res.json();
+  setSalesData(data);
+  setIsLoading(false);
+};
+////////////////////////////////////////////////////////
+
+
+const chartData = {
+  labels: ["Office Supplies", "Technology", "Furniture"],
+  datasets: [
+    {
+      label: "Sales Data",
+      data: [offLen, techLen, furLen],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+      ],
+    },
+  ],
+};
 
 
 // Line chart data
@@ -114,27 +137,6 @@ function App() {
   // //////////////////////////////////////////
 
 
-
-  
-///////////////////////// Fetching data from API  //////////
-
-  const fetchSalesData = async () => {
-    const res = await fetch(
-      "https://g54qw205uk.execute-api.eu-west-1.amazonaws.com/DEV/stub",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ angular_test: "angular-developer" }),
-      }
-    );
-    const data = await res.json();
-    setSalesData(data);
-    setIsLoading(false);
-  };
-  ////////////////////////////////////////////////////////
 
   return (
     <div className='App'>
